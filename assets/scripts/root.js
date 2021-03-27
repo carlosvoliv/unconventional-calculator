d43b124951a8d5367dc5b37763f29c5e57728f6e
Carlos Eduardo Vaz <carlos.voliveira@al.infnet.edu.br>
2021-03-27 15:43:51 -0300

    output function

--- assets/scripts/root.js
+++ assets/scripts/root.js
@@ -3,3 +3,11 @@ const addBtn = document.getElementById('btn-add');
 const subtractBtn = document.getElementById('btn-subtract');
 const multiplyBtn = document.getElementById('btn-multiply');
 const divideBtn = document.getElementById('btn-divide');
+
+const currentResultOutput = document.getElementById('current-result');
+const currentCalculationOutput = document.getElementById('current-calculation');
+
+const outputResult = (result, text) => {
+  currentResultOutput.textContent = result;
+  currentCalculationOutput.textContent = text
+};
